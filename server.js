f5bf70e83624bc2fb4df8a00b277fc2f91b661b4
agusibrahim <pymobile123@gmail.com>
2024-11-27 10:43:46 +0700

    set limit body

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const { PORT } = require('./config.js');
 
 let app = express();
-app.use(express.json());
+app.use(express.json({ limit: '50mb' }));
 app.use(express.static('wwwroot'));
 app.use(require('./routes/auth.js'));
 app.use(require('./routes/models.js'));
